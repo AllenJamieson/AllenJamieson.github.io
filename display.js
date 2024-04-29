bff9f5b59f9a323609a57adea206448da5dab73d
AllenJamieson <allgjam@live.com>
2024-04-29 16:11:25 -0500

    Edit ability to see story and new story

--- display.js
+++ display.js
@@ -28,34 +28,37 @@ let game_list = [
     }
 ];
 
-let story_list = [];
 let img_list = [];
 
 let product_list = [];
 
 let url = document.title.split(' ')[0].toLowerCase();
+console.log(url);
 switch(url) {
     case 'games':
         product_list = game_list
         break;
 }
-
 let display = null;
 
 for (let prod = 0; prod < product_list.length; prod++) {
     product[prod].addEventListener('click', event=> {
-        console.log(event.target.outerHTML.slice(0, 7));
         if(display == null && event.target.outerHTML.slice(0, 7) != '<a href') {
+            console.log("test");
             display = document.createElement('div')
             display.setAttribute('id', "display_product");
-            display.innerHTML = product_list[prod].name + `</br>` + product_list[prod].description;
+            switch(url) {
+                case 'games':
+                    display.innerHTML = product_list[prod].name + `</br>` + product_list[prod].description;
+                    break;
+            }
             document.body.append(display);
         }
     });
 }
 
 addEventListener('click', event=> {
-    if(event.target.outerHTML.slice(0, 6) == '<body>' && display != null) {
+    if(display != null && event.target.outerHTML.includes("<body>")) {
         display.remove();
         display = null;
     }
