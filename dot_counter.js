e5e043da715287c9c68b1cc0d13b35f9f8006aa9
AllenJamieson <allgjam@live.com>
2025-01-22 14:55:41 -0600

    Start on settings section

--- dot_counter.js
+++ dot_counter.js
@@ -6,12 +6,24 @@ let settings_btn = document.getElementById("settings_btn");
 let settings_div = document.getElementById("settings");
 let submit = settings_div.getElementsByTagName("button")[0];
 
+let bool_settings = [
+    'collectContours', 'filterByArea', 'filterByCircularity',
+    'filterByColor', 'filterByConvexity', 'filterByInertia'
+];
+let numeric_settings = [
+    'blobColor', 'maxArea', 'maxCircularity',
+    'maxConvexity', 'maxInertiaRatio', 'maxThreshold',
+    'minArea', 'minCircularity', 'minConvexity',
+    'minDistBetweenBlobs', 'minInertiaRatio', 'minRepeatability',
+    'minThreshold', 'thresholdStep'
+];
+
 let params, detector;
 let scale = 1;
 let color = 4;
 
 access_camera();
-setTimeout(init, 5000); // Creates a delay so that the opencv loads completely
+setTimeout(init, 1000); // Creates a delay so that the opencv loads completely
 settings_div.style.visibility = "hidden"
 settings_btn.addEventListener("click", _=> settings_div.style.visibility = "visible");
 submit.addEventListener("click", settings);
@@ -20,6 +32,7 @@ function init() {
     img_capture_btn.addEventListener("click", capture);
     params = new cv.SimpleBlobDetector().getParams();
     detector = new cv.SimpleBlobDetector();
+    console.log(params);
 }
 
 // https://stackoverflow.com/questions/72420950/how-to-switch-between-front-camera-and-rear-camera-in-javascript
@@ -37,9 +50,8 @@ function handleVideo(cameraFacing) {
 function access_camera() { // TODO check to see how to use outer camera
     navigator.mediaDevices.getUserMedia(handleVideo("environment"))
     .then(stream => {
-        player.srcObject = stream
-        player.play();
-        let {width, height} = stream.getVideoTracks()[0].getSettings()
+        player.srcObject = stream;
+        let {width, height} = stream.getVideoTracks()[0].getSettings();
         player.width = width;
         player.height = height;
     });
@@ -52,7 +64,7 @@ function capture() {
         img_capture_btn.textContent = "O";
     } else {
         player.style.visibility = "hidden";
-        canvas.style.visibility = "visible"
+        canvas.style.visibility = "visible";
         img_capture_btn.textContent = "Back";
         let src = new cv.Mat(player.height, player.width, cv.CV_8UC4);
         new cv.VideoCapture(player).read(src);
@@ -69,17 +81,25 @@ async function count_dots(img, width, height) {
     dot_lbl.textContent = keypoints.size();
     cv.drawKeypoints(img, keypoints, img);
 
-    cv.resize(img, img, new cv.Size(width, height), cv.INTER_LINEAR)
-    cv.imshow(canvas, img)
+    cv.resize(img, img, new cv.Size(width/4, height/4), cv.INTER_LINEAR);
+    cv.imshow(canvas, img);
 }
 
 function settings() {
-    // TODO create the settings for the parameters
-    console.log("HI");
-    let parameters = settings_div.getElementsByTagName("input")
+    let in_params = settings_div.getElementsByClassName("numeric_setting");
+    let bool_params = settings_div.getElementsByClassName("bool_setting");
     
+    for (let bs = 0; bs < numeric_settings.length; bs++) {
+        if(in_params[bs].value != "" && !isNaN(in_params[bs].value))
+            params[numeric_settings[bs]] = parseFloat(in_params[bs].value);
+    }
 
-
+    for (let ns = 0; ns < bool_settings.length; ns++)
+        params[bool_settings[ns]] = bool_params[ns].checked;
+    
     detector = new cv.SimpleBlobDetector(params);
-    settings_div.style.visibility = "hidden"
+    settings_div.style.visibility = "hidden";
+    let scale_div = document.getElementById("scaler");
+    if(scale_div.value != "" && !isNaN(scale_div.value)) scale = parseInt(scale_div.value)
+    console.log(params);
 }
